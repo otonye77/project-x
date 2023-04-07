eb078daf40c1627c75348152b71ac9001be04da5
otonye77 <otonye.amietubodie@decagon.dev>
2023-04-07 20:47:23 +0100

    first commit

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 import "fmt"
 
+//pointers
 func main(){
-	fmt.Println("Hello World")
-	var whatToSay string = "GOOD BYE CRUEL WORLD"
-	fmt.Println(whatToSay)
+	var color string = "Green"
+	fmt.Println(color)
+	pointerpointer(&color)
+	fmt.Println(color)
 }
 
+func pointerpointer(s *string){
+	newvalue := "Red"
+	*s = newvalue
+}
