c165954bacd2bf1e62edf418f33d8a641a684070
otonye77 <otonye.amietubodie@decagon.dev>
2023-04-12 00:43:55 +0100

    master

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
+
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
+func Home(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "This is the home page")
+}
+
 func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello world")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written %d", n))
-	})
+	http.HandleFunc("/", Home)
 	_ = http.ListenAndServe(":8080", nil)
 }
 
