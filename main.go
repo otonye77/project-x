b9546555b0d0e3b2ea6b90812a128c1ed955ddb7
otonye77 <otonye.amietubodie@decagon.dev>
2023-04-11 23:17:00 +0100

    first commit

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
+import "net/http"
 // import ("time")
 import "fmt"
 
+
 func main(){
-	a := []int{90, 10, 50}
-	b := a
-	b[1] = 100
-	fmt.Println(b)
-	fmt.Println(a)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hello world")
+	})
 }
 
