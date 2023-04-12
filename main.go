1706bbd5e885515f1411b708761b3cf6b0b5c0a7
otonye77 <otonye.amietubodie@decagon.dev>
2023-04-12 01:17:03 +0100

    master

--- main.go
+++ main.go
@@ -13,15 +13,17 @@ func Home(w http.ResponseWriter, r *http.Request){
 
 func About(w http.ResponseWriter, r *http.Request){
 	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the sum of 2 + 2 %d", sum))
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the sum of 2 + 2 = %d", sum))
 }
 
-func addValues(x, y, int)int {
+func addValues(x int, y int) int {
 	return x + y
 }
 
 func main(){
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Printf("Starting application on port %s", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
 }
-
