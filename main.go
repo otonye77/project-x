215984a0778fabea40b8f5b163c90e6ea5fe1670
otonye77 <otonye.amietubodie@decagon.dev>
2023-04-12 01:49:54 +0100

    master

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -20,9 +21,28 @@ func addValues(x int, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request){
+	f, err := divideValues(100.0, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by zero you fool")
+		return 
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
+}
+
+func divideValues(x float32, y float32)(float32, error){
+	if y <= 0 {
+		err := errors.New("Cannot divide by 0 you fool")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 func main(){
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Printf("Starting application on port %s", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
