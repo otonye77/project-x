56d61f9f78a386a36f4ce8a772f890c32fc62673
otonye77 <otonye.amietubodie@decagon.dev>
2023-04-12 01:13:20 +0100

    master

--- main.go
+++ main.go
@@ -5,12 +5,23 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
 func Home(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "This is the home page")
 }
 
+func About(w http.ResponseWriter, r *http.Request){
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the sum of 2 + 2 %d", sum))
+}
+
+func addValues(x, y, int)int {
+	return x + y
+}
+
 func main(){
 	http.HandleFunc("/", Home)
-	_ = http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(portNumber, nil)
 }
 
